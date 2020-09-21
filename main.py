8ada270cd36664787f245d062e3ceaf9e8ea6a6f
Antoine HENRIO <antoine.henrio@doqboard.com>
2020-09-21 21:29:22 +0200

    SAUT FONCTIONNEL

--- main.py
+++ main.py
@@ -1,54 +1,45 @@
-import sys, pygame, time
+import sys
+import pygame
+import time
 pygame.init()
- 
+
 size = width, height = 640, 480
 speed = [30, 30]
 white = 255, 255, 255
 screen = pygame.display.set_mode(size)
- 
-dragon = pygame.image.load("./sprites/dragon.png")
-dragon = pygame.transform.scale(dragon, (40,40))
-dragonRect = dragon.get_rect()
-dragonRect.y = height - 40
-pygame.key.set_repeat(100,45)
+isJump = False
 jumpCount = 10
+vel = 20
 
-def monte(objetRect):
-	for x in range(0,speed[0]):
-		objetRect.y -= 1
-		time.sleep(0.01)
-def descend(objetRect):
-	for x in range(0,speed[0]):
-		objetRect.y += 1
-		time.sleep(0.01)
-
+dragon = pygame.image.load("./sprites/dragon.png")
+dragon = pygame.transform.scale(dragon, (40, 40))
+y = height - 40
+x = 0
 while 1:
+    pygame.time.delay(20)
     for event in pygame.event.get():
-    	if event.type == pygame.QUIT: sys.exit()
-    	if event.type == pygame.KEYDOWN:
-    		keys = pygame.key.get_pressed()
-    		if keys[pygame.K_RIGHT]:
-    			if dragonRect.x + 40 + speed[0] < width:
-    				dragonRect = dragonRect.move(speed[0],0)
-    			else:
-    				dragonRect = dragonRect.move(width - 40 - dragonRect.x,0)
-    		if keys[pygame.K_LEFT]:
-    			if dragonRect.x > speed[0]:
-    				dragonRect = dragonRect.move(-1 * speed[0],0)
-    			else:
-    				dragonRect = dragonRect.move(-1 * dragonRect.x,0)
-    		if keys[pygame.K_SPACE]:
-    			# monte(dragonRect)
-    			# time.sleep(1)
-    			# descend(dragonRect)
-    			dragonRect = dragonRect.move(0,-1 * speed[0])
-    			dragonRect = dragonRect.move(0,speed[0])
-    			for x in range(0,speed[0] + 1):
-    				dragonRect = dragonRect.move(0,-1)
-    			screen.blit(dragon, dragonRect)
-    			for y in range(speed[0],-1,-1):
-    				dragonRect = dragonRect.move(0,1)
-    screen.fill(white)
-    screen.blit(dragon, dragonRect)
-    pygame.display.flip()
+        if event.type == pygame.QUIT:
+            sys.exit()
 
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT] and x > vel: 
+        x -= vel
+
+    if keys[pygame.K_RIGHT] and x < width - vel - 40:  
+        x += vel
+
+    if not(isJump):
+        if keys[pygame.K_SPACE]:
+            isJump = True
+    else:
+        if jumpCount >= -10:
+            y -= (jumpCount * abs(jumpCount)) * 0.5
+            jumpCount -= 1
+        else: 
+            jumpCount = 10
+            isJump = False
+
+    screen.fill(white)
+    screen.blit(dragon,(x,y))
+    pygame.display.flip()
